3e752d9f1169dd366f6b8aaaa603fd9a1a0bac51
shan301 <61319149+shan301@users.noreply.github.com>
2024-06-23 13:06:12 +0530

    Add files via upload

---
+++ meaning.py
@@ -0,0 +1,10 @@
+meaning= 8
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('not today')    
+
+#ternary operator
+print('Right on!') if meaning > 10 else print ('not today')
