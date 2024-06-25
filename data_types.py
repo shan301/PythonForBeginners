693e41be4ae53bd5200e35e62b3b3ca28b0bc2fc
shan301 <61319149+shan301@users.noreply.github.com>
2024-06-25 19:03:14 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,55 @@
+#literal assignment
+first="Dave"
+last="Gray"
+
+# print(type(first))
+# print(type(first)==str)
+# print(isinstance(first,str))
+
+#constructor function
+# pizza = str("pepperoni")
+# print(type(pizza))
+# print(type(pizza)==str)
+# print(isinstance(pizza,str))
+
+#Concatenation
+# fullname = first + "" + last
+# print(fullname)
+
+# fullname +="!"
+# print(fullname)
+
+#Casting a number to a string
+# decade = str(1980)
+# print(type(decade))
+# print(decade)
+
+# statement = "I like rock music from the " + decade + "s. "
+# print(statement) 
+
+#Multiple lines
+# multiline='''
+# Hey,how are you?
+
+# I was just checking in.
+#                                                     All good?
+
+# '''
+# print(multiline)
+
+#Escaping specail characters
+# sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\located'
+# print(sentence)
+
+#String methods
+
+# print(first)
+# print(first.lower())
+# print(first.upper())
+# print(first)
+
+
+
+
+
+
